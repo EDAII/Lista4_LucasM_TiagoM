34873ad00f06cb1105f7e19e72b9d3b8c64a52d6
Tiago Miguel <tmcstiago@gmail.com>
2019-05-09 21:45:52 -0300

    Iniciando projeto

---
+++ projeto4.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Veiculo {
+
+    public:
+        string placa;
+        string montadora;
+        string modelo;
+        int ano_modelo;
+        string estado;
+        string proprietario;
+
+        Veiculo(string placa, string montadora, string modelo, int ano_modelo, string stado, string proprietario){
+            this->placa = placa;
+            this->montadora = montadora;
+            this->modelo = modelo;
+            this->ano_modelo = ano_modelo;
+            this->estado = estado;
+            this->proprietario = proprietario;
+        }
+
+};
+
+int main () {
+
+    vector<Veiculo> veiculos;
+
+    
+
+
+    return 0;
+}
