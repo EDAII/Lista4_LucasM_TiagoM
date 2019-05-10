e489adf686bd387327ea2972f949767df06d59ff
medeiroslucas <medeirosrosalucas@gmail.com>
2019-05-09 22:43:02 -0300

    Criar função de sort com radix

--- projeto4.cpp
+++ projeto4.cpp
@@ -21,14 +21,59 @@ class Veiculo {
             this->proprietario = proprietario;
         }
 
+        Veiculo(string placa){
+            this->placa = placa;
+        }
+
 };
+
+vector<Veiculo> radix(vector<Veiculo> veiculos){
+
+    queue<Veiculo> index [43];
+
+    for(int i=6; i>=0; i--){
+
+        for(auto p:veiculos){
+            index[p.placa[i] - '0'].push(p);
+        }
+
+        veiculos.clear();
+
+        for(int j=0; j<43; j++){
+            while(index[j].size()){
+                veiculos.push_back(index[j].front());
+                index[j].pop();
+            }
+        }
+    }
+    return veiculos;
+}
 
 int main () {
 
     vector<Veiculo> veiculos;
 
-    
+    Veiculo aux("ZZZ9999");
 
+    veiculos.push_back(aux);
+
+    aux.placa = "ZZZ9990";
+
+    veiculos.push_back(aux);
+
+    aux.placa = "ZZA9990";
+
+    veiculos.push_back(aux);
+    
+    aux.placa = "AAA0000";
+
+    veiculos.push_back(aux);
+    
+    veiculos = radix(veiculos);
+    
+    for(auto p:veiculos){
+        cout << p.placa << endl;
+    }
 
     return 0;
 }
